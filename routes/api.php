2bc910f356c1e3f9fef74b0e02d0ebdfea3ab379
Anderson Belderrama <andersonbelderrama@gmail.com>
2024-05-11 18:25:52 -0300

    api test router user login

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,19 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DebtController;
 use App\Http\Controllers\RecurrentExpenseController;
 use App\Http\Controllers\TransactionController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// Route::get('health', function () {
+//     return response()->json([
+//         'status' => 'ok',
+//         'version' => config('app.version'),
+// ]);
+// })->middleware('auth:sanctum');
+
+Route::get('user', function (Request $request) {
+    return $request->user();
+});
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('accounts', AccountController::class);
